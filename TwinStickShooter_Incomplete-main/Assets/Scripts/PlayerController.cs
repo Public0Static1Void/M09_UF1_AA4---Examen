9d3cfa35b766e6e6e656d46757f30bb1e55ba23b
Daniel Gonzalez Peralta <daniel.gonzalez@enti.cat>
2024-11-05 16:31:15 +0100

    Player movement

--- TwinStickShooter_Incomplete-main/Assets/Scripts/PlayerController.cs
+++ TwinStickShooter_Incomplete-main/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float speedRotation;
     public Transform target;
+
+    private Rigidbody rb;
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
-    void Update()
-    {        
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        Vector3 mov = new Vector3(horizontal, 0, vertical) * speed;
+        Vector3 vel = transform.TransformDirection(mov);
+
+        rb.velocity = new Vector3(vel.x, rb.velocity.y, vel.z);
     }
 }
