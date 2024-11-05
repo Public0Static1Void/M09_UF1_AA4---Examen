b3d917bb8ad67bde702d79de6018b291182bcf14
Daniel Gonzalez Peralta <daniel.gonzalez@enti.cat>
2024-11-05 16:45:23 +0100

    Enemy rotation

--- TwinStickShooter_Incomplete-main/Assets/Scripts/EnemyController.cs
+++ TwinStickShooter_Incomplete-main/Assets/Scripts/EnemyController.cs
@@ -7,13 +7,17 @@ public class EnemyController : MonoBehaviour
 {
     public float speedRotation;
     public float stoppingDistance;
+
+    [SerializeField] private Transform player;
     private void Start()
     {
 
     }
     private void Update()
     {
-
+        Vector3 dir = player.position - transform.position;
+        Quaternion rot = Quaternion.LookRotation(dir, transform.up);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, rot, speedRotation * Time.deltaTime);
     }
 
     public void Kill()
